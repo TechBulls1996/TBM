ba28fa4d3093bb4782390cbbb0421f49f16e55cd
Surya Pratap <soroutlove1996@gmail.com>
2024-01-11 13:27:43 +0530

    add constant and services

---
+++ frontend/src/services/authServices.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { API_URL } from '../helpers/constant';
+
+export const UserLogin = async (params) => {
+  try {
+    const res = await axios.post(`${API_URL}/auth/login`, params);
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const UserRegister = async (params) => {
+  try {
+    const res = await axios.post(`${API_URL}/auth/register`, params);
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
